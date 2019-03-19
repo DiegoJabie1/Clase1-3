43d69b42977e6db3a7df177ba69018e91a2bbd86
DiegoJabie1 <47386362+DiegoJabie1@users.noreply.github.com>
2019-03-19 10:33:48 -0300

    Add files via upload

---
+++ Clase2/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   int numeros=0;
+   int sumaNumeros=0;
+   char s=s;
+   do
+   {
+   printf("Ingrese un numero: ");
+   scanf("%d",numeros);
+
+   printf("\nSi quiere seguir ingrese s: ");
+   scanf("%s",s);
+
+   sumaNumeros=sumaNumeros+numeros;
+
+   }while(s==s);
+
+   printf("\nLa suma de sus numeros dio: ",sumaNumeros);
+
+   /*int i;
+
+   for(i=0;i<10;i++)
+   {
+    if(i==5)
+    {
+        continue;
+    }
+    printf("%d-",i);
+   }*/
+
+   /*int i;
+
+   for(i=0;i<10;i++)
+   {
+       printf("%d-",i+1);
+   }
+   printf("\nEste es el valor de i: %d",i);*/
+
+
+   /*har caracter;
+    char palabra[16];
+
+    printf("Ingrese una palabra: ");
+    scanf("%s",palabra);
+
+    printf("Usted ingreso %s",palabra);
+
+    int numero;
+    int segundoNumero;
+    int suma;
+    float promedio;
+
+    printf("Ingrese un numero: ");
+    scanf("%d",&numero);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d",&segundoNumero);
+
+    suma= numero + segundoNumero;
+
+    promedio= (float)suma/2;
+
+    printf("El promedio es: %.2f",promedio);
+
+    system("pause");*/
+
+    return 0;
+}
